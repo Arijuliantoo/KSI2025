45b4ffd55d0322f8245fd2cfecf82f555fa36f7c
Arijuliantoo <juliantoari350@gmail.com>
2025-11-03 13:56:56 +0700

    Menambahkan fitur tambah data mahasiswa

--- tambah.php
+++ tambah.php
@@ -1,23 +1,20 @@
 <?php
 include 'koneksi.php';
 
-if (isset($_POST['submit'])) {
-    $nim  = $_POST['nim'];
-    $nama = $_POST['nama'];
-    $prodi = $_POST['prodi'];
+if (isset($_POST['simpan'])) {
+    $nim     = $_POST['nim'];
+    $nama    = $_POST['nama'];
+    $jurusan = $_POST['jurusan'];
+    $alamat  = $_POST['alamat'];
+    $no_hp   = $_POST['no_hp'];
 
-    $query = "INSERT INTO mahasiswa (nim, nama, prodi) VALUES ('$nim', '$nama', '$prodi')";
-    mysqli_query($koneksi, $query);
-    echo "Data berhasil ditambahkan!";
+    $sql = "INSERT INTO mahasiswa (nim, nama, jurusan, alamat, no_hp)
+            VALUES ('$nim', '$nama', '$jurusan', '$alamat', '$no_hp')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "<script>alert('Data berhasil ditambahkan!'); window.location='index.php';</script>";
+    } else {
+        echo "Error: " . $conn->error;
+    }
 }
 ?>
-
-<form method="post">
-  <label>NIM:</label><br>
-  <input type="text" name="nim"><br>
-  <label>Nama:</label><br>
-  <input type="text" name="nama"><br>
-  <label>Prodi:</label><br>
-  <input type="text" name="prodi"><br><br>
-  <button type="submit" name="submit">Tambah Data</button>
-</form>
